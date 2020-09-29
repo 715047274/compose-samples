c1180cd51da8bae2d22ba61e338886a3220dcd61
Nick Butcher <nickbutcher@google.com>
2020-09-29 13:36:53 +0100

    WIP: [Jetsnack] Use drawWithCache for gradients.
    Change-Id: I1d3cfc494c1c710d9bb627692d276677a106e9d5

--- Jetsnack/app/src/main/java/com/example/jetsnack/ui/components/Gradient.kt
+++ Jetsnack/app/src/main/java/com/example/jetsnack/ui/components/Gradient.kt
@@ -25,8 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
-import androidx.compose.ui.drawWithContent
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.drawWithCache
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.HorizontalGradient
@@ -40,50 +39,29 @@ import androidx.compose.ui.unit.dp
 
 fun Modifier.horizontalGradientBackground(
     colors: List<Color>
-) = gradientBackground(colors) { gradientColors, size ->
-    HorizontalGradient(
-        colors = gradientColors,
-        startX = 0f,
-        endX = size.width
+) = drawWithCache {
+    val gradient = HorizontalGradient(
+        startX = 0.0f,
+        endX = size.width,
+        colors = colors
     )
+    onDraw {
+        drawRect(brush = gradient)
+    }
 }
 
 fun Modifier.diagonalGradientTint(
     colors: List<Color>,
     blendMode: BlendMode
-) = gradientTint(colors, blendMode) { gradientColors, size ->
-    LinearGradient(
-        colors = gradientColors,
+) = drawWithCache {
+    val gradient = LinearGradient(
+        colors = colors,
         startX = 0f,
         startY = 0f,
         endX = size.width,
         endY = size.height
     )
-}
-
-fun Modifier.gradientBackground(
-    colors: List<Color>,
-    brushProvider: (List<Color>, Size) -> LinearGradient
-): Modifier = composed {
-    var size by remember { mutableStateOf(Size.Zero) }
-    val gradient = remember(colors, size) { brushProvider(colors, size) }
-    drawWithContent {
-        size = this.size
-        drawRect(brush = gradient)
-        drawContent()
-    }
-}
-
-fun Modifier.gradientTint(
-    colors: List<Color>,
-    blendMode: BlendMode,
-    brushProvider: (List<Color>, Size) -> LinearGradient
-) = composed {
-    var size by remember { mutableStateOf(Size.Zero) }
-    val gradient = remember(colors, size) { brushProvider(colors, size) }
-    drawWithContent {
-        drawContent()
-        size = this.size
+    onDraw {
         drawRect(
             brush = gradient,
             blendMode = blendMode
